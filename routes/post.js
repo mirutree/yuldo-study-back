29ba38535f5c5c0dab5293a8791fbb5c5d70af8f
mirutree <wonyiwonyi@naver.com>
2022-01-13 00:18:14 +0900

    임시로 user_seq = 1로 넣어줌

--- routes/post.js
+++ routes/post.js
@@ -9,39 +9,38 @@ const { QueryTypes } = require("sequelize");
 router.get("/", async (req, res) => {
   try {
     // 로그인 중인지 확인한다.
-    // 차단 된 사람인지 확인한다.
+    // 차단 된 사람인지 확인한다
 
     // 카테고리 확인한다.
     const { category } = req.query;
 
     let sql = "";
-    if (!category) { // 카테고리가 없으면 (전체 카테고리면)
-        sql = "SELECT * FROM `tb_board` WHERE state = 'Y'"; // 디비설계할 떄부터 state가 N이어야 삭제된 글 Y = 삭제안된 글
+    if (!category) {
+      // 전체 카테고리
+      sql = "SELECT * FROM `tb_board` WHERE state = 'Y'";
     } else {
-        sql = "SELECT * FROM `tb_board` WHERE category = ? AND state = 'Y'";
+      sql = "SELECT * FROM `tb_board` WHERE category = ? AND state = 'Y'";
     }
-    // db에서 카테고리 글을 불러온다.
+    // db에서 카테고리 글을 불러온다
     const results = await db.sequelize.query(sql, {
-      replacements: [ category ],
+      replacements: [category],
       type: QueryTypes.SELECT,
     });
     // 클라이언트로 데이터를 넘겨준다.
-    res.status(200).json({ result: 1, data: results }); // 성공일 경우 result가 1
-    // 디비의 리턴값을 확인한다
-    // 상태코드 혹은 데이터를 클라이언트에 리턴한다.
+    res.status(200).json({ result: 1, data: results });
   } catch (e) {
     console.error(e);
-    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다." });
+    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다" });
   }
 });
 
 // 글 하나 보기
 router.get("/:board_seq", async (req, res) => {
   try {
-    // 로그인 중인지 확인한다. -> 차단되어 있으면 return
-    // 글 번호를 받아와서
-    // 글 번호에 맞는 글 1개를 가져온다.
-    const { board_seq } = req.params; // 위 :board_seq변수를 그대로 가져옴
+    // 로그인 중인지 확인한다. -> 차단되어있으면 return
+    // 글번호를 받아와서
+    // 글번호에 맞는 글 1개를 가져온다.
+    const { board_seq } = req.params;
 
     let sql = "SELECT * FROM `tb_board` WHERE seq = ? AND state = 'Y'";
     const results = await db.sequelize.query(sql, {
@@ -50,11 +49,9 @@ router.get("/:board_seq", async (req, res) => {
     });
     // 클라이언트로 데이터를 넘겨준다.
     res.status(200).json({ result: 1, data: results });
-    // 디비의 리턴값을 확인한다
-    // 상태코드 혹은 데이터를 클라이언트에 리턴한다.
   } catch (e) {
     console.error(e);
-    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다." });
+    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다" });
   }
 });
 
@@ -63,55 +60,55 @@ router.post("/", async (req, res) => {
   try {
     // 프론트에서 받은 데이터
     const { title, contents, writer } = req.body;
-
     // 데이터의 유효성을 확인한다.
     if (!title || !contents || !writer) {
       res
-      .status(401)
-      .json({ result: -1, data: null, err: "유효하지 않은 데이터 입니다." });
+        .status(401)
+        .json({ result: -1, data: null, err: "유효하지 않은 데이터 입니다." });
     }
     // 유저가 로그인 중인지 확인한다. -> 로그인 구현하면 확인
+    const user_seq = 1;
 
     // 디비에 넣어준다.
-    const [ results, metadata ] = await db.sequelize.query(
-      "INSERT INTO `tb_board`(user_seq, category, title, contents, writer) VALUES(1, 'F', ?, ?, ?)",
+    const [results, metadata] = await db.sequelize.query(
+      "INSERT INTO `tb_board`(user_seq, category, title, contents, writer) VALUES(?, 'F', ?, ?, ?)",
       {
-        replacements: [ title, contents, writer ],
+        replacements: [user_seq, title, contents, writer],
         type: QueryTypes.INSERT,
       }
     );
     res.status(200).json({ result: 1, data: results });
+    // 디비의 리턴값을 확인한다
+    // 상태코드 혹은 데이터를 클라이언트에 리턴한다.
   } catch (e) {
     console.error(e);
-    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다." });
+    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다" });
   }
 });
 
-// 글 수정
+// 수정
 router.put("/:board_seq", async (req, res) => {
   try {
-    // 로그인 중인지 확인한다. -> 차단된 유저면 return
-    // 로그인 중인 유저의 번호를 가져온다.
+    // 로그인 중인지 확인한다 -> 차단된 유저면 return
+    // 로그인 중인 유저의 번호를 가져온다
     let user_seq = 1;
     const { board_seq } = req.params;
-
-    // 유저가 작성자인지 확인한다.
-    let sql = 
+    // 이 유저가 글쓴이인지 확인한다.
+    let sql =
       "SELECT COUNT(*) as cnt FROM `tb_board` WHERE user_seq = ? AND seq = ?";
-
-    const results = await db.sequelize.query(sql, {
-      replacements: [ user_seq, board_seq ],
+    const [results] = await db.sequelize.query(sql, {
+      replacements: [user_seq, board_seq],
       type: QueryTypes.SELECT,
     });
-    // 유저 = 작성자면 수정 아니면 return
+    // 글쓴이가 맞으면 수정 아니면 return
 
     if (parseInt(results.cnt) > 0) {
       const { title, contents, category } = req.body;
 
-      let sql = 
+      let sql =
         "UPDATE `tb_board` SET title = ?, contents = ?, category = ? WHERE seq = ?";
-      const [ results, metadata ] = await db.sequelize.query(sql, {
-        replacements: [ title, contents, category, board_seq ],
+      const [results, metadata] = await db.sequelize.query(sql, {
+        replacements: [title, contents, category, board_seq],
         type: QueryTypes.UPDATE,
       });
 
@@ -119,27 +116,25 @@ router.put("/:board_seq", async (req, res) => {
     }
   } catch (e) {
     console.error(e);
-    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다." });
+    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다" });
   }
 });
 
-// 글 삭제
+// 글삭제
 router.delete("/:board_seq", async (req, res) => {
   try {
-    // 로그인 중인지 확인한다. -> 차단된 유저면 return
+    // 로그인 중인지 확인한다 -> 차단된 유저면 return
     // 로그인 중인 유저의 번호를 가져온다
     let user_seq = 1;
     const { board_seq } = req.params;
-
-    // 유저가 작성자인지 확인한다.
-    let sql = 
+    // 이 유저가 글쓴이인지 확인한다.
+    let sql =
       "SELECT COUNT(*) as cnt FROM `tb_board` WHERE user_seq = ? AND seq = ?";
-
-    const results = await db.sequelize.query(sql, {
-      replacements: [ user_seq, board_seq ],
+    const [results] = await db.sequelize.query(sql, {
+      replacements: [user_seq, board_seq],
       type: QueryTypes.SELECT,
     });
-    // 유저 = 작성자이면 삭제 아니면 return
+    // 글쓴이가 맞으면 삭제 아니면 return
 
     if (parseInt(results.cnt) > 0) {
       let sql = "UPDATE SET `tb_board` state = 'N' WHERE seq = ?";
@@ -150,30 +145,35 @@ router.delete("/:board_seq", async (req, res) => {
 
       res.status(200).json({ result: 1, data: result });
     } else {
-      res.status(401).json({ result: -1, data: null, err: "존재하지 않는 글 이거나 글쓴이가 아닙니다." });
+      res.status(401).json({
+        result: -1,
+        data: null,
+        err: "존재하지 않는 글 이거나 글쓴이가 아닙니다.",
+      });
     }
   } catch (e) {
     console.error(e);
-    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다." });
+    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다" });
   }
 });
 
-// 게시글 검색
+// 검색
 router.get("/:type/:search", async (req, res) => {
   try {
-    // 로그인 중인지 확인한다. -> 차단된 유저면 return
+    // 로그인 된 유저인지 확인한다 -> 차단된 유저면 return
     // 카테고리, search string 확인
     const { search, type } = req.params;
     let { category } = req.query;
 
     if (!search) {
-      res.status(401).json({ result: -1, data: null, err: "검색어를 입력 해주세요." });
+      res
+        .status(401)
+        .json({ result: -1, data: null, err: "검색어를 입력 해주세요" });
     }
-
     // 타입이 없으면 제목 + 내용
     let sql = "";
     if (type === "title") {
-      sql = `SELECT * FROM tb_board WHERE title LIKE :search AND state = 'Y'`; // like검색을 할때는 ?가 안먹힘
+      sql = `SELECT * FROM tb_board WHERE title LIKE :search AND state = 'Y'`;
     } else if (type === "contents") {
       sql = `SELECT * FROM tb_board WHERE contents LIKE :search AND state = 'Y'`;
     } else {
@@ -181,26 +181,28 @@ router.get("/:type/:search", async (req, res) => {
       sql = `SELECT * FROM tb_board WHERE title LIKE :search OR contents LIKE :search AND state = 'Y'`;
     }
 
-    if (category) { // 카테고리가 있는 경우
+    if (category) {
       sql = sql + " AND category = :category";
     }
 
     sql = sql + " ORDER BY ins_dttm DESC";
 
-    // 카테고리가 없으면 전체 카테고리를 검색한다.
+    // 카테고리가 없으면 전체 카테고를 검색한다.
     const result = await db.sequelize.query(sql, {
       replacements: { search: "%" + search + "%", category },
       type: QueryTypes.SELECT,
     });
 
     if (result.length === 0) {
-      res.status(200).json({ result: -2, data: null, err: "검색 결과가 없습니다." });
+      res
+        .status(200)
+        .json({ result: -2, data: null, err: "검색 결과가 없습니다" });
     } else {
       res.status(200).json({ result: 1, data: result });
     }
   } catch (e) {
     console.error(e);
-    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다." });
+    res.status(500).json({ result: -100, data: null, err: "서버 에러 입니다" });
   }
 });
 
